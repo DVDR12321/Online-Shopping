0e4e1903c2ee9b9b9dd90d2c81702f1811e28f9a
unknown <danilovukovic.95@gmail.com>
2023-08-19 00:08:10 +0200

    Context and logic for adding cart items

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,8 +1,15 @@
+import { useContext } from 'react';
+
+import { CartContext } from '../../context/cart.context';
+
 import './product-card.styles.scss'
 import Button from "../button/button.component";
 
 const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
+  const { addItemToCart } = useContext(CartContext);
+
+  const addProductToCart = () => addItemToCart(product);
 
   return (
     <div className="product-card-container">
@@ -10,7 +17,7 @@ const ProductCard = ({ product }) => {
       <div className="footer">
         <span className="name">{name}</span>
         <span className="cost">{price}</span>
-        <Button buttonType="inverted"> Add to cart </Button>
+        <Button buttonType="inverted" onClick = {addProductToCart}> Add to cart </Button>
       </div>
     </div>
   );
